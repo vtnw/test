2a1d062f33643d587a945974050a90861037f716
vtnw <vtnw@users.noreply.github.com>
2017-06-10 13:38:13 +0530

    Add files via upload

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,17 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+            .register('./sw.js')
+            .then(function() 
+            { console.log('Service Worker Registered'); 
+            });
+}
 
+function add() {
+    var data = '';
+    if (navigator.onLine) {
+        document.getElementById('dvContent').innerHTML = 'Successfully Added';
+    }
+    else {
+        document.getElementById('dvContent').innerHTML = 'Unable to connect..';
+    }
+};
