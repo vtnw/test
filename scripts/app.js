9c56f7378d5d97b5c2b55cf77142a4cf9f11d52b
vtnw <vtnw@users.noreply.github.com>
2017-06-11 15:42:43 +0530

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -6,12 +6,22 @@ if ('serviceWorker' in navigator) {
             });
 }
 
-function add() {
-    var data = '';
-    if (navigator.onLine) {
-        document.getElementById('dvContent').innerHTML = 'Successfully Added';
-    }
-    else {
-        document.getElementById('dvContent').innerHTML = 'Unable to connect..';
-    }
-};
+	var xhr;
+  	document.getElementById("btnTranslate").onclick = function(event) {
+  		var source = document.getElementById("tbSource").value;
+  		var lang = document.getElementById("ddlLang").options[document.getElementById("ddlLang").selectedIndex].value;
+  		alert(source);alert(lang);
+	 	xhr = new XMLHttpRequest();
+ 		var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=" + lang + "&dt=t&q=" + source;
+ 		xhr.open('GET', url, true);
+ 		xhr.send();
+ 
+ 		xhr.onreadystatechange = processRequest;
+ 	}
+ 
+ 	function processRequest(e) {
+ 		if (xhr.readyState == 4 && xhr.status == 200) {
+ 			var response = JSON.parse(xhr.responseText);
+ 			document.getElementById("tbTarget").value = response[0][0][0];
+ 		}
+ 	}
