183e75c6f6122a47d527c036b1d881131d02da2c
vtnw <vtnw@users.noreply.github.com>
2018-10-16 16:17:41 +0530

    Update simplenotify.js

--- simplenotify.js
+++ simplenotify.js
@@ -1,7 +1,7 @@
 function show() {
-    return navigator.serviceWorker.register('service-worker.js')
+    return navigator.serviceWorker.register('sw2.js')
     .then(function(registration) {
-      registration.showNotification('Hello 2', { body: 'Nice'});
+      registration.showNotification('Hello 3', { body: 'Nice'});
     })
     .catch(function(err) {
       console.error('Unable to register service worker.', err);
