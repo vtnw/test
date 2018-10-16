98f7d9c2950c403f8e9272f96aed11e46f7ed72e
vtnw <vtnw@users.noreply.github.com>
2018-10-16 16:00:34 +0530

    Update simplenotify.js

--- simplenotify.js
+++ simplenotify.js
@@ -1,22 +1,11 @@
 function show(){
-  alert('hi4');
+  alert('hi5');
   return registerServiceWorker()
     .then(function(registration) {
-    const title = 'Web Push Book';
+    const title = 'Pass 1';
     const options = {
       body: 'This would be the body text of the notification.\n' +
         'It can hold two lines of text.',
-      icon: '/images/demos/icon-512x512.png',
-      badge: '/images/demos/badge-128x128.png',
-      image: '/images/demos/unsplash-farzad-nazifi-1600x1100.jpg',
-      tag: 'example-notification',
-      actions: [
-        {
-          action: 'download-book-action',
-          title: 'Download Book',
-          icon: '/images/demos/action-download-book-128x128.png'
-        }
-      ]
     };
     registration.showNotification(title, options);
   });
